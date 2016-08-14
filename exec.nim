5adacbf66f6afcd285cc460ba1ed96dff8547ade
Andrey Platov <andrey@xored.com>
2016-08-14 19:23:35 +0700

    before work on ops

--- exec.nim
+++ exec.nim
@@ -8,22 +8,14 @@ import strutils
 var vm = createVM()
 vm.bootstrap(vm.makeNatives)
 
-let ob = makeObject(vm)
-echo "ob: ", toHex(cast[int](ob.head))
-var user = vm.alloc (ob)
-
-echo "user: ", toHex(cast[int](user))
+var user = vm.alloc makeObject(vm)
 
 proc interpret(s: string) =
   let b = vm.parse s
   echo "evaluating:", b
-  vm.dumpHeap 0, 20
   vm.expandAll(b, user)
-  vm.dumpHeap 0, 20
   vm.bindAll(b, user)
-  vm.dumpHeap(0, 20)
   echo "EVAL: ", vm.eval(b) 
-  vm.dumpHeap 0, 30
 
 # let code = if paramCount() > 0: readFile paramStr(1)
 #            else: readAll stdin
